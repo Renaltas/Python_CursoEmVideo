0a3500de120339128347464289163e277c231684
Renaltas <pedromoucamm@gmail.com>
2023-11-13 16:42:52 -0300

    Mundo2 até o exercicio 62 hehe

---
+++ Mundo2Python/Desafio61.py
@@ -0,0 +1,11 @@
+print("faremos uma PA")
+pt = int(input("insira o primeiro termo!"))
+r = int(input("insiira a razão!"))
+termo = pt
+c = 0
+while c <= 10:
+    termo += r
+    print(termo)
+    c+=1
+
+#ajuda do gunabara hehe
---
+++ Mundo2Python/Desafio62.py
@@ -0,0 +1,39 @@
+print("faremos uma PA")
+pt = int(input("insira o primeiro termo!"))
+r = int(input("insiira a razão!"))
+termo = pt
+c = 0
+total = 0 
+mais = 10
+
+while mais != 0:
+    total = total + mais
+    while c <= total:
+        termo += r
+        c+= 1
+    print("PAUSA")
+    mais = int(input("quantos termos a mais vc vai querer?"))
+print("FIM")
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#Slk exercicio dificil da poha ta maluco 
+#Resovido pelo guanabara ta maluco o cara é muito bom 
+    
+    
+    
+
+
+
+
+ 
